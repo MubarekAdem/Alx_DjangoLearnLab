cefc93a5ac20899f3f6386774223f9196c7fdc8d
MubarekAdem <mubarekadem001@gmail.com>
2025-09-10 12:18:49 +0300

    task1

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("books/", views.list_books_view, name="list_books"),
     path("register/", views.register_view, name="register"),
+ path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),
+
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.views.generic import DetailView
 
-# Create your views here.
+# Class-based view to show library details
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = "relationship_app/library_detail.html"
+    context_object_name = "library"
