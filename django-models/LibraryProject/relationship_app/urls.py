f60363b1782cc5030e41faccf7706e9edfd4eb53
MubarekAdem <mubarekadem001@gmail.com>
2025-09-10 14:45:10 +0300

    fixed urls and views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,20 +1,21 @@
-
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views
 from django.urls import path
 from .views import (
     list_books_view,
     LibraryDetailView,
     register_view,
-    login_view,
-    logout_view,
+
 )
 
 
 urlpatterns = [
-    path("books/", list_books_view, name="list_books"),
-    path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
-
-   
-     path("register/", register_view, name="register"),
-    path("login/", login_view, name="login"),
-    path("logout/", logout_view, name="logout"),
-]
+     path("login/", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
+    path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
+    
+    # Function-based registration view
+    path("register/", views.register_view, name="register"),
+    
+    # Other existing views
+    path("books/", views.list_books_view, name="list_books"),
+    path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),
